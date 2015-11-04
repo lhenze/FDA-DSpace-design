77fbf97a2f43c62a2cf007272be0e219fffad8d3
Laura Henze <lh435@nyu.edu>
2015-11-04 16:30:03 -0500

    cleaning up includes, breadcrumbs

--- jspui/inc/containerbanner.php
+++ jspui/inc/containerbanner.php
@@ -1,9 +1,10 @@
-<div class="container banner">
+<!-- <div class="container banner">
     <div class="row">
       <div class="col-md-9 brand">
         <h1>banner</h1>
         <h4>DSpace preserves and enables easy and open access to all types of digital content including text, images, moving images, mpegs and data sets</h4>
-       <!-- <a href="http://www.dspace.org" class="btn btn-primary">Learn More</a>--> </div>
+    <a href="http://www.dspace.org" class="btn btn-primary">Learn More</a></div>
       <div class="col-md-3">&nbsp; </div>
     </div>
   </div>
+  --> 

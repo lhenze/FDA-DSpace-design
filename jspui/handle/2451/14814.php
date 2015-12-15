c36504d8614fff648aa46d64416480070d4f21ad
Laura Henze <laurahenze@gmail.com>
2015-12-15 16:08:35 -0500

    FDA tree added for a Community page

--- jspui/handle/2451/14814.php
+++ jspui/handle/2451/14814.php
@@ -28,6 +28,34 @@
   <script src="/ux/fda/static/js/html5shiv.js"></script>
   <script src="/ux/fda/static/js/respond.min.js"></script>
 <![endif]-->
+  <script type="text/javascript">
+  var jQ = jQuery.noConflict();
+  jQ(document).ready(function() {
+    jQ(".fda-tree li").each(function(index) {
+      if (jQ(this).find("ul").length) {
+        jQ(this).addClass("has-sublist");
+        jQ(this).find("ul").hide();
+        var ArrowElement = jQ('<a class="arrow" href="#">+</a>');
+        jQ(this).prepend(ArrowElement);
+        ArrowElement.click(function(event) {
+          event.preventDefault();
+          openstate = jQ(this).siblings("ul").is(":visible");
+          if (!openstate) {
+            jQ(this).text("-");
+            jQ(this).addClass("isOpen");
+            jQ(this).siblings("ul").slideDown("fast", function() {});
+          } else {
+            jQ(this).removeClass("isOpen");
+            jQ(this).siblings("ul").slideUp("fast");
+            jQ(this).text("+");
+          }
+        });
+      } else {
+        jQ(this).addClass("has-no-sublist");
+      }
+    });
+  });
+  </script>
 </head>
 
 <body class="undernavigation">
@@ -37,77 +65,29 @@
       <?php include "../../inc/containerbanner.php";  ?>
         <div class="container">
           <ol class="breadcrumb btn-success">
-            <li><a href="../../index.php">FDA home</a></li>
+            <li><a href="../../index.php">Faculty Digital Archive</a></li>
             <li><a href="../../community-list.php">Communities and Collections</a></li>
             <li>Arts and Science</li>
           </ol>
         </div>
         <div class="container">
-          <div class="page-title-area">
-            <div class="row">
-              <div class="col-md-8">
-                <h2>Arts and Science <small>Community home page</small>  </h2>
-                <p>Descriptive Text - optional. Eros elementum proin nunc molestie quis parturient habitasse sodales est suspendisse ridiculus at non a a parturient commodo et phasellus a ante elit blandit.Metus lorem suspendisse a lobortis a natoque consequat tristique nibh consectetur a sem adipiscing nostra mi morbi purus maecenas eros rutrum rutrum himenaeos ornare aenean egestas lorem dapibus ad.Himenaeos a a consequat bibendum ad a tellus dictumst turpis facilisi praesent tincidunt nec felis ullamcorper a parturient.</p>
-            
-               <?php include "../../inc/searchcommunity.php";  ?>
-                
-              
-                <?php include "../../inc/collectionsincommunity.php";  ?>
-              </div>
-              <div class="col-md-4">
-                <?php   include "../../inc/viewstatsforthiscollection.php";  ?>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="container">
           <div class="row">
-            <div class="col-md-12">
-              <h3 class="facets">Discover</h3>
-              <div id="facets" class="facetsBox row panel">
-                <div id="facet_author" class="facet col-md-4"> <span class="facetName">Author</span>
-                  <ul class="list-group">
-                    <li class="list-group-item"><span class="badge">73</span> <a href="/ux/fda/simple-searchCollins.php" title="Filter by Collins, Chris"> Collins, Chris</a></li>
-                    <li class="list-group-item"><span class="badge">72</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=Namaseb%2C+Levi&amp;filtername=author&amp;filtertype=equals" title="Filter by Namaseb, Levi"> Namaseb, Levi</a></li>
-                    <li class="list-group-item"><span class="badge">27</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=Unknown%2C+Unknown&amp;filtername=author&amp;filtertype=equals" title="Filter by Unknown, Unknown"> Unknown, Unknown</a></li>
-                    <li class="list-group-item"><span class="badge">19</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=Unknown&amp;filtername=author&amp;filtertype=equals" title="Filter by Unknown"> Unknown</a></li>
-                    <li class="list-group-item"><span class="badge">15</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=New+York+Department+of+Sanitation&amp;filtername=author&amp;filtertype=equals" title="Filter by New York Department of Sanitation"> New York Department of Sanitation</a></li>
-                    <li class="list-group-item"><span class="badge">12</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=Kuhn%2C+Jeremy&amp;filtername=author&amp;filtertype=equals" title="Filter by Kuhn, Jeremy"> Kuhn, Jeremy</a></li>
-                    <li class="list-group-item"><span class="badge">11</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=Martin%2C+Sean&amp;filtername=author&amp;filtertype=equals" title="Filter by Martin, Sean"> Martin, Sean</a></li>
-                    <li class="list-group-item"><span class="badge">10</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=Chodroff%2C+Eleanor&amp;filtername=author&amp;filtertype=equals" title="Filter by Chodroff, Eleanor"> Chodroff, Eleanor</a></li>
-                    <li class="list-group-item"><span class="badge">10</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=Kaufman%2C+Brianna&amp;filtername=author&amp;filtertype=equals" title="Filter by Kaufman, Brianna"> Kaufman, Brianna</a></li>
-                    <li class="list-group-item"><span class="badge">10</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=Regalado%2C+Nancy+Freeman&amp;filtername=author&amp;filtertype=equals" title="Filter by Regalado, Nancy Freeman"> Regalado, Nancy Freeman</a></li>
-                    <li class="list-group-item"><span style="visibility: hidden;">.</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814?author_page=1"><span class="pull-right">next ></span></a> </li>
-                  </ul>
-                </div>
-                <div id="facet_subject" class="facet col-md-4"> <span class="facetName">Subject</span>
-                  <ul class="list-group">
-                    <li class="list-group-item"><span class="badge">70</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=x&amp;filtername=subject&amp;filtertype=equals" title="Filter by x"> x</a></li>
-                    <li class="list-group-item"><span class="badge">39</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=History&amp;filtername=subject&amp;filtertype=equals" title="Filter by History"> History</a></li>
-                    <li class="list-group-item"><span class="badge">37</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=September+11&amp;filtername=subject&amp;filtertype=equals" title="Filter by September 11"> September 11</a></li>
-                    <li class="list-group-item"><span class="badge">36</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=Sanitation+worker&amp;filtername=subject&amp;filtertype=equals" title="Filter by Sanitation worker"> Sanitation worker</a></li>
-                    <li class="list-group-item"><span class="badge">35</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=World+Trade+Center&amp;filtername=subject&amp;filtertype=equals" title="Filter by World Trade Center"> World Trade Center</a></li>
-                    <li class="list-group-item"><span class="badge">34</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=On+the+Job%2C+work&amp;filtername=subject&amp;filtertype=equals" title="Filter by On the Job, work"> On the Job, work</a></li>
-                    <li class="list-group-item"><span class="badge">32</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=Trash+and+Transformation&amp;filtername=subject&amp;filtertype=equals" title="Filter by Trash and Transformation"> Trash and Transformation</a></li>
-                    <li class="list-group-item"><span class="badge">30</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=On+the+Job%2C+social&amp;filtername=subject&amp;filtertype=equals" title="Filter by On the Job, social"> On the Job, social</a></li>
-                    <li class="list-group-item"><span class="badge">25</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=Manhattan&amp;filtername=subject&amp;filtertype=equals" title="Filter by Manhattan"> Manhattan</a></li>
-                    <li class="list-group-item"><span class="badge">22</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=Special+event&amp;filtername=subject&amp;filtertype=equals" title="Filter by Special event"> Special event</a></li>
-                    <li class="list-group-item"><span style="visibility: hidden;">.</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814?subject_page=1"><span class="pull-right">next ></span></a> </li>
-                  </ul>
-                </div>
-                <div id="facet_dateIssued" class="facet col-md-4"> <span class="facetName">Date issued</span>
-                  <ul class="list-group">
-                    <li class="list-group-item"><span class="badge">163</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=%5B2010+TO+2015%5D&amp;filtername=dateIssued&amp;filtertype=equals" title="Filter by 2010 - 2015"> 2010 - 2015</a></li>
-                    <li class="list-group-item"><span class="badge">225</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=%5B2000+TO+2009%5D&amp;filtername=dateIssued&amp;filtertype=equals" title="Filter by 2000 - 2009"> 2000 - 2009</a></li>
-                    <li class="list-group-item"><span class="badge">1</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=%5B1990+TO+1999%5D&amp;filtername=dateIssued&amp;filtertype=equals" title="Filter by 1990 - 1999"> 1990 - 1999</a></li>
-                    <li class="list-group-item"><span class="badge">1</span> <a href="http://msdlib.home.nyu.edu/ux/fda/handle/2451/14814/simple-search?filterquery=%5B1967+TO+1969%5D&amp;filtername=dateIssued&amp;filtertype=equals" title="Filter by 1967 - 1969"> 1967 - 1969</a></li>
-                  </ul>
-                </div>
+            <div class="col-md-8">
+              <div class="page-title-area">
+                <h2>Arts and Science <small>Community home page</small>  </h2>
               </div>
+              <?php include "../../inc/searchcommunity.php";  ?>
+                <section class="collections-list">
+                  <h3>Collections and sub-communities</h3>
+                  <div class="fda-tree">
+                    <?php include "../../inc/collectionsincommunity.php";  ?>
+                  </div>
+                </section>
+            </div>
+            <div class="col-md-4">
+              &nbsp;
             </div>
           </div>
-          <div class="row"> </div>
-
         </div>
     </main>
     <?php include "../../inc/footer.php";  ?>
--- jspui/inc/collectionsincommunity.php
+++ jspui/inc/collectionsincommunity.php
@@ -1,92 +1,94 @@
-<div class="subcommunities">
-  <h3>Sub-communities within this community</h3>
-  <div class="list-group">
-    <div class="list-group-item ">
-      <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/28317"> Department of Linguistics Field Methods Course</a> </div>
-      <p class="collectionDescription"></p>
-    </div>
-    <div class="list-group-item ">
-      <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/29657"> Professor Christopher Collins Fieldwork Archive</a> </div>
-      <p class="collectionDescription"></p>
-    </div>
-    <div class="list-group-item ">
-      <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/27717"> Program in Museum Studies, GSAS</a> </div>
-      <p class="collectionDescription">MA Theses</p>
-    </div>
-  </div>
-  <h3>Collections in this community</h3>
-  <div class="list-group">
-    <div class="list-group-item ">
-      <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/27826"> David Ludden Archive</a> </div>
-      <p class="collectionDescription"></p>
-    </div>
-    <div class="list-group-item ">
-      <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/31466"> Debraj Ray's Papers</a> </div>
-      <p class="collectionDescription"></p>
-    </div>
-    <div class="list-group-item ">
-      <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/27490"> Facial expressions video collection</a> </div>
-      <p class="collectionDescription"></p>
-    </div>
-    <div class="list-group-item ">
-      <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/33574"> Gillian Gallagher's collection</a> </div>
-      <p class="collectionDescription"></p>
-    </div>
-    <div class="list-group-item ">
-      <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/33722"> Heeger Lab Collection</a> </div>
-      <p class="collectionDescription"></p>
-    </div>
-    <div class="list-group-item ">
-      <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/33725"> Jonathan Winawer’s Collection</a> </div>
-      <p class="collectionDescription"></p>
-    </div>
-    <div class="list-group-item ">
-      <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/29798"> Louise Rice's Publications</a> </div>
-      <p class="collectionDescription"></p>
-    </div>
-    <div class="list-group-item ">
-      <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/31677"> Lucas Champollion's publications</a> </div>
-      <p class="collectionDescription"></p>
-    </div>
-    <div class="list-group-item ">
-      <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/31612"> Maria Gouskova's Collection</a> </div>
-      <p class="collectionDescription"></p>
-    </div>
-    <div class="list-group-item ">
-      <div>
-        <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/23350"> NYU-New York Department of Sanitation Museum Project</a> </div>
-        <p class="collectionDescription"></p>
-      </div>
-    </div>
-    <div class="list-group-item ">
-      <div>
-        <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/28070"> Nancy F Regalado Collection</a> </div>
-        <p class="collectionDescription"></p>
-      </div>
-    </div>
-    <div class="list-group-item ">
-      <div>
-        <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/14815"> Professor Persell's Collection</a> </div>
-        <p class="collectionDescription"></p>
-      </div>
-    </div>
-    <div class="list-group-item ">
-      <div>
-        <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/31989"> Shaghayegh Harbi Collection</a> </div>
-        <p class="collectionDescription"></p>
-      </div>
-    </div>
-    <div class="list-group-item ">
-      <div>
-        <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/33770"> VAAAMP image repository</a> </div>
-        <p class="collectionDescription"></p>
-      </div>
-    </div>
-    <div class="list-group-item ">
-      <div>
-        <div class="list-group-item-heading"><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/33881"> test shenhard</a> </div>
-        <p class="collectionDescription"></p>
-      </div>
-    </div>
-  </div>
-</div>
+
+  <ul>
+     <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/28317">Department of Linguistics Field Methods Course</a> </h4>
+      <ul>
+        <li>
+          <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/29947">Field Methods Fall 2011 - Kazakh</a> </h4>
+        </li>
+        <li>
+          <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/29676">Field Methods Spring 2005 - Kabiye</a> </h4>
+        </li>
+        <li>
+          <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/29655">Field Methods Spring 2007 - Mortlockese</a> </h4>
+        </li>
+        <li>
+          <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/29656">Field Methods Spring 2008 - Shupamem</a> </h4>
+        </li>
+        <li>
+          <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/28320">Field Methods Spring 2009 - Medumba</a> </h4>
+        </li>
+        <li>
+          <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/28318">Field Methods Spring 2010 - Masarak</a> </h4>
+        </li>
+        <li>
+          <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/29898">Field Methods Spring 2011 - Kyerepong</a> </h4>
+        </li>
+        <li>
+          <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/31705">Field Methods Spring 2013 - Songhay</a> </h4>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/29657">Professor Christopher Collins Fieldwork Archive</a> </h4>
+      <ul>
+        <li>
+          <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/29658">=Hoan Language Collection</a> </h4>
+        </li>
+        <li>
+          <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/29659">N|uu Language Collection</a> </h4>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/27717">Program in Museum Studies, GSAS</a> </h4>
+      <ul>
+        <li>
+          <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/27720">Museum Studies MA Theses Collection</a> </h4>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/27826">David Ludden Archive</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/31466">Debraj Ray's Papers</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/27490">Facial expressions video collection</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/33574">Gillian Gallagher's collection</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/33722">Heeger Lab Collection</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/33725">Jonathan Winawer’s Collection</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/29798">Louise Rice's Publications</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/31677">Lucas Champollion's publications</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/31612">Maria Gouskova's Collection</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/23350">NYU-New York Department of Sanitation Museum Project</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/28070">Nancy F Regalado Collection</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/14815">Professor Persell's Collection</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/31989">Shaghayegh Harbi Collection</a> </h4>
+    </li>
+    <li>
+      <h4><a href="http://msdlib.home.nyu.edu/jspui/handle/2451/33770">VAAAMP image repository</a> </h4>
+    </li>
+   
+  </ul>

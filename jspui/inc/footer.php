abad6b67f070981a80bd5f3e7f3438cde1807722
Laura Henze <laurahenze@gmail.com>
2015-12-18 13:12:46 -0500

    Footer: markup and CSS

--- jspui/inc/footer.php
+++ jspui/inc/footer.php
@@ -1,9 +1,6 @@
- <footer class="navbar navbar-inverse navbar-bottom">
-      <div id="designedby" class="container text-muted"> 
-        <div id="footer_feedback" class="center">
-          <p class="text-muted"><a target="_blank" href="#">Link to Static Page</a> | <a target="_blank" href="#">Link to Static Page</a> | <a target="_blank" href="#">Link to Static Page</a> | Powered by&nbsp; <a target="_blank" href="http://www.duraspace.org/">Duraspace</a>
-        
-          </p>
-        </div>
-      </div>
-    </footer>
+<footer class="navbar navbar-inverse">
+  <div class="footer-holder container">
+    <div class="f-1"><a target="_blank" href="#">About the Archive</a> | <a target="_blank" href="#">Help</a> | <a target="_blank" href="#">Contact</a></div>
+    <div class="f-2">Powered by <a target="_blank" href="http://dlib.nyu.edu/dlts//">NYU DLTS</a> and <a target="_blank" href="http://dlib.nyu.edu/dlts//">DSpace</a> </div>
+  </div>
+</footer>

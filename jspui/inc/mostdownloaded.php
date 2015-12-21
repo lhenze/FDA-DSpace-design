3e1165cd2f85a35838aa23b242494c945cfef654
Laura Henze <laurahenze@gmail.com>
2015-12-21 14:48:45 -0500

    markup for most downloaded

---
+++ jspui/inc/mostdownloaded.php
@@ -0,0 +1,31 @@
+<article >
+  <div class="communityflag"><span>Community:</span> <a href="#">Arts and Science</a></div>
+  <h1><a href="#">Properties of pattern and component direction selective cells in area MT of the macaque</a></h1>
+  <div class="authors"><a class="author" href="/browse?type=author&amp;value=Wang%2C+H+X">Wang, H X</a>, <a class="author" href="/browse?type=author&amp;value=Movshon%2C+J+A">Movshon, J A</a></div>
+</article>
+<article >
+  <div class="communityflag"><span>Community:</span> <a href="#">Division of Libraries</a></div>
+  <h1><a href="#"> The role of education and research in enhancing rural women's income and household happiness</a></h1>
+  <div class="authors"><a class="author" href="/browse?type=author&amp;value=Wang%2C+H+X">Swaminathan, M.S.</a></div>
+</article>
+<article >
+  <div class="communityflag"><span>Community:</span> <a href="#">NYU School of Medicine</a></div>
+  <h1><a href="#">Methotrexate Prevents Wear Particle-induced Inflammatory Osteolysis in Mice via Activation of Adenosine A2A Receptor</a></h1>
+  <div class="authors"><a class="author" href="/browse?type=author&amp;value=Mediero%2C+Aranzazu">Mediero, Aranzazu</a>, <a class="author" href="/browse?type=author&amp;value=Perez-Aso%2C+Miguel">Perez-Aso, Miguel</a>, <a class="author" href="/browse?type=author&amp;value=Wilder%2C+Tuere">Wilder, Tuere</a>, <a class="author" href="/browse?type=author&amp;value=Cronstein%2C+Bruce+N.">Cronstein, Bruce N.</a></div>
+</article>
+<article >
+  <div class="communityflag"><span>Community:</span> <a href="#">Arts and Science</a></div>
+  <h1><a href="#">Properties of pattern and component direction selective cells in area MT of the macaque</a></h1>
+  <div class="authors"><a class="author" href="/browse?type=author&amp;value=Wang%2C+H+X">Wang, H X</a>, <a class="author" href="/browse?type=author&amp;value=Movshon%2C+J+A">Movshon, J A</a></div>
+</article>
+<article >
+  <div class="communityflag"><span>Community:</span> <a href="#">Division of Libraries</a></div>
+  <h1><a href="#">The role of education and research in enhancing rural women's income and household happiness</a></h1>
+  <div class="authors"><a class="author" href="/browse?type=author&amp;value=Wang%2C+H+X">Swaminathan, M.S.</a></div>
+</article>
+<article >
+  <div class="communityflag"><span>Community:</span> <a href="#">NYU School of Medicine</a></div>
+  <h1><a href="#">Methotrexate Prevents Wear Particle-induced Inflammatory Osteolysis in Mice via Activation of Adenosine A2A Receptor</a></h1>
+  <div class="authors"><a class="author" href="/browse?type=author&amp;value=Mediero%2C+Aranzazu">Mediero, Aranzazu</a>, <a class="author" href="/browse?type=author&amp;value=Perez-Aso%2C+Miguel">Perez-Aso, Miguel</a>, <a class="author" href="/browse?type=author&amp;value=Wilder%2C+Tuere">Wilder, Tuere</a>, <a class="author" href="/browse?type=author&amp;value=Cronstein%2C+Bruce+N.">Cronstein, Bruce N.</a></div>
+
+</article><!-- <a class="viewmore" href="#">View more</a> -->

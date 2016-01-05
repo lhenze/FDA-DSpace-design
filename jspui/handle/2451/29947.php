dd28367fd10820b86bd9d1641956a3ffabd20714
Laura Henze <laurahenze@gmail.com>
2016-01-05 17:30:33 -0500

    Changes for "facet" panel
    updated classes to be more like the other panels
    wrap panel header in appropriate tags
    move the “badge” element inside the anchor tag

--- jspui/handle/2451/29947.php
+++ jspui/handle/2451/29947.php
@@ -53,7 +53,6 @@
               This collection contains materials on the Kazakh language, spoken in Kazakhstan, northern Soviet Middle Asia into western Siberia. The materials were gathered during the Fall 2011 NYU Field Methods Course, taught by Professor Maria Gouskova. If you are interested in accessing the materials in this database, please contact Maria Gouskova at the Department of Linguistics at NYU.
             </div>
             <p class="copyrightText"></p>
-         
             <div class="row">
             </div>
             <div class="row">
@@ -239,69 +238,71 @@
                 </form>
               </div>
             </div>
-            <div class="col-md-12">
-              <h3 class="facets">Discover</h3>
-              <div id="facets" class="facetsBox row panel">
-                <div id="facet_author" class="facet col-md-12">
+            <div class="panel">
+              <div class="panel-heading">Discover</div>
+              <div id="facets" class="facetsBox panel-body">
+                <div id="facet_author" class="facet">
                   <span class="facetName">Author</span>
                   <ul class="list-group">
-                    <li class="list-group-item"><span class="badge">12</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Kuhn%2C+Jeremy&amp;filtername=author&amp;filtertype=equals" title="Filter by Kuhn, Jeremy">
-                  Kuhn, Jeremy</a></li>
-                    <li class="list-group-item"><span class="badge">11</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Martin%2C+Sean&amp;filtername=author&amp;filtertype=equals" title="Filter by Martin, Sean">
+                    <li class="list-group-item"><a href="/jspui/handle/2451/29947/simple-search?filterquery=Kuhn%2C+Jeremy&amp;filtername=author&amp;filtertype=equals" title="Filter by Kuhn, Jeremy"><span class="badge">12</span>
+                  Kuhn, Jeremy </a></li>
+                    <li class="list-group-item"> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Martin%2C+Sean&amp;filtername=author&amp;filtertype=equals" title="Filter by Martin, Sean"><span class="badge">11</span>
                   Martin, Sean</a></li>
-                    <li class="list-group-item"><span class="badge">10</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Chodroff%2C+Eleanor&amp;filtername=author&amp;filtertype=equals" title="Filter by Chodroff, Eleanor">
+                    <li class="list-group-item"><a href="/jspui/handle/2451/29947/simple-search?filterquery=Chodroff%2C+Eleanor&amp;filtername=author&amp;filtertype=equals" title="Filter by Chodroff, Eleanor"><span class="badge">10</span>
                   Chodroff, Eleanor</a></li>
-                    <li class="list-group-item"><span class="badge">10</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Kaufman%2C+Brianna&amp;filtername=author&amp;filtertype=equals" title="Filter by Kaufman, Brianna">
-                  Kaufman, Brianna</a></li>
-                    <li class="list-group-item"><span class="badge">9</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Lowy%2C+Maya&amp;filtername=author&amp;filtertype=equals" title="Filter by Lowy, Maya">
+                    <li class="list-group-item"><a href="/jspui/handle/2451/29947/simple-search?filterquery=Kaufman%2C+Brianna&amp;filtername=author&amp;filtertype=equals" title="Filter by Kaufman, Brianna"><span class="badge">10</span>
+                  Kaufman, Brianna </a></li>
+                    <li class="list-group-item"><a href="/jspui/handle/2451/29947/simple-search?filterquery=Lowy%2C+Maya&amp;filtername=author&amp;filtertype=equals" title="Filter by Lowy, Maya"><span class="badge">9</span>
                   Lowy, Maya</a></li>
-                    <li class="list-group-item"><span class="badge">6</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Gouskova%2C+Maria&amp;filtername=author&amp;filtertype=equals" title="Filter by Gouskova, Maria">
+                    <li class="list-group-item"><a href="/jspui/handle/2451/29947/simple-search?filterquery=Gouskova%2C+Maria&amp;filtername=author&amp;filtertype=equals" title="Filter by Gouskova, Maria"><span class="badge">6</span> 
                   Gouskova, Maria</a></li>
-                    <li class="list-group-item"><span class="badge">5</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Al+Kaabi%2C+Meera&amp;filtername=author&amp;filtertype=equals" title="Filter by Al Kaabi, Meera">
+                    <li class="list-group-item"> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Al+Kaabi%2C+Meera&amp;filtername=author&amp;filtertype=equals" title="Filter by Al Kaabi, Meera"><span class="badge">5</span>
                   Al Kaabi, Meera</a></li>
-                    <li class="list-group-item"><span class="badge">5</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Kasyanenko%2C+Sonia&amp;filtername=author&amp;filtertype=equals" title="Filter by Kasyanenko, Sonia">
+                    <li class="list-group-item"> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Kasyanenko%2C+Sonia&amp;filtername=author&amp;filtertype=equals" title="Filter by Kasyanenko, Sonia"><span class="badge">5</span>
                   Kasyanenko, Sonia</a></li>
-                    <li class="list-group-item"><span class="badge">4</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Lee%2C+Sang-Im&amp;filtername=author&amp;filtertype=equals" title="Filter by Lee, Sang-Im">
+                    <li class="list-group-item"> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Lee%2C+Sang-Im&amp;filtername=author&amp;filtertype=equals" title="Filter by Lee, Sang-Im"><span class="badge">4</span>
                   Lee, Sang-Im</a></li>
-                    <li class="list-group-item"><span class="badge">2</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Al+kaabi%2C+Meera&amp;filtername=author&amp;filtertype=equals" title="Filter by Al kaabi, Meera">
-                  Al kaabi, Meera</a></li>
-                    <li class="list-group-item"><span style="visibility: hidden;">.</span>
+                    <li class="list-group-item"><a href="/jspui/handle/2451/29947/simple-search?filterquery=Al+kaabi%2C+Meera&amp;filtername=author&amp;filtertype=equals" title="Filter by Al kaabi, Meera"><span class="badge">2</span>
+                  Al kaabi, Meera </a></li>
+                    <li class="list-group-item">
                       <a href="/jspui/handle/2451/29947?author_page=1"><span class="pull-right">next ></span></a>
                     </li>
                   </ul>
                 </div>
-                <div id="facet_subject" class="facet col-md-12">
+                <div id="facet_subject" class="facet">
                   <span class="facetName">Subject</span>
                   <ul class="list-group">
-                    <li class="list-group-item"><span class="badge">2</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=comparatives&amp;filtername=subject&amp;filtertype=equals" title="Filter by comparatives">
-                  comparatives</a></li>
-                    <li class="list-group-item"><span class="badge">2</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Kazakh&amp;filtername=subject&amp;filtertype=equals" title="Filter by Kazakh">
-                  Kazakh</a></li>
-                    <li class="list-group-item"><span class="badge">2</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Kazakh%2C+field+methods%2C+class&amp;filtername=subject&amp;filtertype=equals" title="Filter by Kazakh, field methods, class">
+                    <li class="list-group-item"> <a href="/jspui/handle/2451/29947/simple-search?filterquery=comparatives&amp;filtername=subject&amp;filtertype=equals" title="Filter by comparatives">
+                  comparatives<span class="badge">2</span></a></li>
+                    <li class="list-group-item"><a href="/jspui/handle/2451/29947/simple-search?filterquery=Kazakh&amp;filtername=subject&amp;filtertype=equals" title="Filter by Kazakh">
+                  Kazakh<span class="badge">2</span> </a></li>
+                    <li class="list-group-item"> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Kazakh%2C+field+methods%2C+class&amp;filtername=subject&amp;filtertype=equals" title="Filter by Kazakh, field methods, class"><span class="badge">2</span>
                   Kazakh, field methods, class</a></li>
-                    <li class="list-group-item"><span class="badge">2</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=morphology&amp;filtername=subject&amp;filtertype=equals" title="Filter by morphology">
+                    <li class="list-group-item"> <a href="/jspui/handle/2451/29947/simple-search?filterquery=morphology&amp;filtername=subject&amp;filtertype=equals" title="Filter by morphology"><span class="badge">2</span>
                   morphology</a></li>
-                    <li class="list-group-item"><span class="badge">1</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=accusative+case&amp;filtername=subject&amp;filtertype=equals" title="Filter by accusative case">
+                    <li class="list-group-item"><a href="/jspui/handle/2451/29947/simple-search?filterquery=accusative+case&amp;filtername=subject&amp;filtertype=equals" title="Filter by accusative case"><span class="badge">2</span>
                   accusative case</a></li>
-                    <li class="list-group-item"><span class="badge">1</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=adjective+ordering&amp;filtername=subject&amp;filtertype=equals" title="Filter by adjective ordering">
+                    <li class="list-group-item"><a href="/jspui/handle/2451/29947/simple-search?filterquery=adjective+ordering&amp;filtername=subject&amp;filtertype=equals" title="Filter by adjective ordering"><span class="badge">1</span> 
                   adjective ordering</a></li>
-                    <li class="list-group-item"><span class="badge">1</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=adjective+suffixes%2C+determiners%2C+syntax&amp;filtername=subject&amp;filtertype=equals" title="Filter by adjective suffixes, determiners, syntax">
+                    <li class="list-group-item"><a href="/jspui/handle/2451/29947/simple-search?filterquery=adjective+suffixes%2C+determiners%2C+syntax&amp;filtername=subject&amp;filtertype=equals" title="Filter by adjective suffixes, determiners, syntax"><span class="badge">1</span> 
                   adjective suffixes, determiners, ...</a></li>
-                    <li class="list-group-item"><span class="badge">1</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=agreement&amp;filtername=subject&amp;filtertype=equals" title="Filter by agreement">
-                  agreement</a></li>
-                    <li class="list-group-item"><span class="badge">1</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Basic+syntactic+order&amp;filtername=subject&amp;filtertype=equals" title="Filter by Basic syntactic order">
+                    <li class="list-group-item">
+                      <a href="/jspui/handle/2451/29947/simple-search?filterquery=agreement&amp;filtername=subject&amp;filtertype=equals" title="Filter by agreement"> <span class="badge">1</span> agreement
+                      </a>
+                    </li>
+                    <li class="list-group-item"> <a href="/jspui/handle/2451/29947/simple-search?filterquery=Basic+syntactic+order&amp;filtername=subject&amp;filtertype=equals" title="Filter by Basic syntactic order"><span class="badge">1</span>
                   Basic syntactic order</a></li>
-                    <li class="list-group-item"><span class="badge">1</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=case+markers%2C+wh-word+in+the+embedded+clause&amp;filtername=subject&amp;filtertype=equals" title="Filter by case markers, wh-word in the embedded clause">
+                    <li class="list-group-item"><a href="/jspui/handle/2451/29947/simple-search?filterquery=case+markers%2C+wh-word+in+the+embedded+clause&amp;filtername=subject&amp;filtertype=equals" title="Filter by case markers, wh-word in the embedded clause"><span class="badge">1</span> 
                   case markers, wh-word in the embe...</a></li>
-                    <li class="list-group-item"><span style="visibility: hidden;">.</span>
+                    <li class="list-group-item">
                       <a href="/jspui/handle/2451/29947?subject_page=1"><span class="pull-right">next ></span></a>
                     </li>
                   </ul>
                 </div>
-                <div id="facet_dateIssued" class="facet col-md-12">
+                <div id="facet_dateIssued" class="facet col-md-12x">
                   <span class="facetName">Date issued</span>
                   <ul class="list-group">
-                    <li class="list-group-item"><span class="badge">71</span> <a href="/jspui/handle/2451/29947/simple-search?filterquery=2011&amp;filtername=dateIssued&amp;filtertype=equals" title="Filter by 2011">
+                    <li class="list-group-item"> <a href="/jspui/handle/2451/29947/simple-search?filterquery=2011&amp;filtername=dateIssued&amp;filtertype=equals" title="Filter by 2011"><span class="badge">71</span>
                   2011</a></li>
                   </ul>
                 </div>

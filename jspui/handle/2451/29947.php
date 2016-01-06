cac18c70eaac77a52b2b303ee55e10b41e209880
Laura Henze <laurahenze@gmail.com>
2016-01-06 14:10:13 -0500

    fixing some incorrect nesting

--- jspui/handle/2451/29947.php
+++ jspui/handle/2451/29947.php
@@ -32,170 +32,36 @@
 <body>
   <a class="sr-only" href="#content">Skip navigation</a>
   <?php include "../../inc/header.php";  ?>
-    <div class="container">
+   
+    <main id="content" role="main">
+       <div class="container">
       <ol class="breadcrumb btn-success">
         <li><a href="/jspui/">Faculty Digital Archive</a></li>
         <li><a href="/jspui/handle/2451/14814">Arts and Science</a></li>
         <li><a href="/jspui/handle/2451/28317">Department of Linguistics Field Methods Course</a></li>
       </ol>
     </div>
-    <main id="content" role="main">
       <div class="container">
         <div class="row">
           <div class="col-md-9">
             <header>
-              <div class="row">
-                <div class="col-md-8">
-                  <h2>Field Methods Fall 2011 - Kazakh</h2></div>
-              </div>
-              This collection contains materials on the Kazakh language, spoken in Kazakhstan, northern Soviet Middle Asia into western Siberia. The materials were gathered during the Fall 2011 NYU Field Methods Course, taught by Professor Maria Gouskova. If you are interested in accessing the materials in this database, please contact Maria Gouskova at the Department of Linguistics at NYU.
+              <h2>Field Methods Fall 2011 - Kazakh</h2> This collection contains materials on the Kazakh language, spoken in Kazakhstan, northern Soviet Middle Asia into western Siberia. The materials were gathered during the Fall 2011 NYU Field Methods Course, taught by Professor Maria Gouskova. If you are interested in accessing the materials in this database, please contact Maria Gouskova at the Department of Linguistics at NYU.
             </header>
             <p class="copyrightText"></p>
-            <div class="row">
-            </div>
-            <div class="row">
-              <div class="col-md-8">
-                <div class="panel panel-simple-search">
-                  <div class="panel-heading">Search within this collection:</div>
-                  <div class="panel-body">
-                    <form method="get" action="/jspui/simple-search" class="simplest-search">
-                      <div class="form-group-flex">
-                        <div class="input-hold">
-                          <input type="text" class="form-control" placeholder="Titles, authors, keywords..." name="query" id="tequery">
-                        </div>
-                        <div class="button-hold">
-                          <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span></button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
+            <?php include "../../inc/searchcollection.php";  ?>
+              <div class="browse_range">
+                Collection&#39;s Items (Sorted by Submit Date in Descending order): 1 to 20 of 71
               </div>
-            </div>
-            <div class="browse_range">
-              Collection&#39;s Items (Sorted by Submit Date in Descending order): 1 to 20 of 71
-            </div>
-            <div class="prev-next-links">
-              &nbsp;<a href="/jspui/handle/2451/29947?offset=20">next&nbsp;&gt;</a>
-            </div>
-            <table align="center" class="table" summary="This table browses all dspace content">
-              <colgroup>
-                <col width="130" />
-                <col width="60%" />
-                <col width="40%" />
-              </colgroup>
-              <tr>
-                <th id="t1" class="oddRowEvenCol">Issue Date</th>
-                <th id="t2" class="oddRowOddCol">Title</th>
-                <th id="t3" class="oddRowEvenCol">Author(s)</th>
-              </tr>
-              <tr>
-                <td headers="t1" class="evenRowEvenCol" nowrap="nowrap" align="right">21-Dec-2011</td>
-                <td headers="t2" class="evenRowOddCol"><a href="/jspui/handle/2451/31416">Field&#x20;Session&#x20;2011-12-14</a></td>
-                <td headers="t3" class="evenRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Gouskova%2C+Maria">Gouskova,&#x20;Maria</a>; <a href="/jspui/browse?type=author&amp;value=Kasyanenko%2C+Sonia">Kasyanenko,&#x20;Sonia</a>; <a href="/jspui/browse?type=author&amp;value=Kasyanenko%2C+Sonia">Kasyanenko,&#x20;Sonia</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raikhan">Beksultanova,&#x20;Raikhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="oddRowEvenCol" nowrap="nowrap" align="right">15-Dec-2011</td>
-                <td headers="t2" class="oddRowOddCol"><a href="/jspui/handle/2451/31379">Field&#x20;Methods&#x20;2011-10-12</a></td>
-                <td headers="t3" class="oddRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Kuhn%2C+Jeremy">Kuhn,&#x20;Jeremy</a>; <a href="/jspui/browse?type=author&amp;value=Martin%2C+Sean">Martin,&#x20;Sean</a>; <a href="/jspui/browse?type=author&amp;value=Kuhn%2C+Jeremy">Kuhn,&#x20;Jeremy</a>; <a href="/jspui/browse?type=author&amp;value=Martin%2C+Sean">Martin,&#x20;Sean</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="evenRowEvenCol" nowrap="nowrap" align="right">15-Dec-2011</td>
-                <td headers="t2" class="evenRowOddCol"><a href="/jspui/handle/2451/31378">Field&#x20;Methods&#x20;2011-12-07</a></td>
-                <td headers="t3" class="evenRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Kaufman%2C+Brianna">Kaufman,&#x20;Brianna</a>; <a href="/jspui/browse?type=author&amp;value=Lowy%2C+Maya">Lowy,&#x20;Maya</a>; <a href="/jspui/browse?type=author&amp;value=Martin%2C+Sean">Martin,&#x20;Sean</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="oddRowEvenCol" nowrap="nowrap" align="right">15-Dec-2011</td>
-                <td headers="t2" class="oddRowOddCol"><a href="/jspui/handle/2451/31377">Field&#x20;Methods&#x20;2011-12-12</a></td>
-                <td headers="t3" class="oddRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Martin%2C+Sean">Martin,&#x20;Sean</a>; <a href="/jspui/browse?type=author&amp;value=Kaufman%2C+Brianna">Kaufman,&#x20;Brianna</a>; <a href="/jspui/browse?type=author&amp;value=Chodroff%2C+Eleanor">Chodroff,&#x20;Eleanor</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="evenRowEvenCol" nowrap="nowrap" align="right">14-Dec-2011</td>
-                <td headers="t2" class="evenRowOddCol"><a href="/jspui/handle/2451/31376">Field&#x20;Session&#x20;2011-11-16</a></td>
-                <td headers="t3" class="evenRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Chodroff%2C+Eleanor">Chodroff,&#x20;Eleanor</a>; <a href="/jspui/browse?type=author&amp;value=Chodroff%2C+Eleanor">Chodroff,&#x20;Eleanor</a>; <a href="/jspui/browse?type=author&amp;value=Lowy%2C+Maya">Lowy,&#x20;Maya</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="oddRowEvenCol" nowrap="nowrap" align="right">10-Dec-2011</td>
-                <td headers="t2" class="oddRowOddCol"><a href="/jspui/handle/2451/31345">Field&#x20;Sessions&#x20;2011-12-07</a></td>
-                <td headers="t3" class="oddRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Lowy%2C+Maya">Lowy,&#x20;Maya</a>; <a href="/jspui/browse?type=author&amp;value=Chodroff%2C+Eleanor">Chodroff,&#x20;Eleanor</a>; <a href="/jspui/browse?type=author&amp;value=Lowy%2C+Maya">Lowy,&#x20;Maya</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="evenRowEvenCol" nowrap="nowrap" align="right">9-Dec-2011</td>
-                <td headers="t2" class="evenRowOddCol"><a href="/jspui/handle/2451/31344">Field&#x20;Sessions&#x20;2011-11-30</a></td>
-                <td headers="t3" class="evenRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Lowy%2C+Maya">Lowy,&#x20;Maya</a>; <a href="/jspui/browse?type=author&amp;value=Chodroff%2C+Eleanor">Chodroff,&#x20;Eleanor</a>; <a href="/jspui/browse?type=author&amp;value=Lowy%2C+Maya">Lowy,&#x20;Maya</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="oddRowEvenCol" nowrap="nowrap" align="right">6-Dec-2011</td>
-                <td headers="t2" class="oddRowOddCol"><a href="/jspui/handle/2451/31342">Field&#x20;Methods&#x20;2011-12-05</a></td>
-                <td headers="t3" class="oddRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Lowy%2C+Maya">Lowy,&#x20;Maya</a>; <a href="/jspui/browse?type=author&amp;value=Al+Kaabi%2C+Meera">Al&#x20;Kaabi,&#x20;Meera</a>; <a href="/jspui/browse?type=author&amp;value=Kaufman%2C+Brianna">Kaufman,&#x20;Brianna</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="evenRowEvenCol" nowrap="nowrap" align="right">6-Dec-2011</td>
-                <td headers="t2" class="evenRowOddCol"><a href="/jspui/handle/2451/31340">Field&#x20;Session&#x20;2011-11-21</a></td>
-                <td headers="t3" class="evenRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Kaufman%2C+Brianna">Kaufman,&#x20;Brianna</a>; <a href="/jspui/browse?type=author&amp;value=Kaufman%2C+Brianna">Kaufman,&#x20;Brianna</a>; <a href="/jspui/browse?type=author&amp;value=Lee%2C+Sang-Im">Lee,&#x20;Sang-Im</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="oddRowEvenCol" nowrap="nowrap" align="right">4-Dec-2011</td>
-                <td headers="t2" class="oddRowOddCol"><a href="/jspui/handle/2451/31338">Field&#x20;Methods&#x20;2011-11-30</a></td>
-                <td headers="t3" class="oddRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Al+Kaabi%2C+Meera">Al&#x20;Kaabi,&#x20;Meera</a>; <a href="/jspui/browse?type=author&amp;value=Lee%2C+Sang-Im">Lee,&#x20;Sang-Im</a>; <a href="/jspui/browse?type=author&amp;value=Lowy%2C+Maya">Lowy,&#x20;Maya</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="evenRowEvenCol" nowrap="nowrap" align="right">1-Dec-2011</td>
-                <td headers="t2" class="evenRowOddCol"><a href="/jspui/handle/2451/31337">Field&#x20;Session&#x20;2011-11-24</a></td>
-                <td headers="t3" class="evenRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Kasyanenko%2C+Sonia">Kasyanenko,&#x20;Sonia</a>; <a href="/jspui/browse?type=author&amp;value=Kasyanenko%2C+Sonia">Kasyanenko,&#x20;Sonia</a>; <a href="/jspui/browse?type=author&amp;value=Al+Kaabi%2C+Meera">Al&#x20;Kaabi,&#x20;Meera</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raikhan">Beksultanova,&#x20;Raikhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="oddRowEvenCol" nowrap="nowrap" align="right">1-Dec-2011</td>
-                <td headers="t2" class="oddRowOddCol"><a href="/jspui/handle/2451/31334">Kazakh&#x20;fricatives</a></td>
-                <td headers="t3" class="oddRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Al+kaabi%2C+Meera">Al&#x20;kaabi,&#x20;Meera</a>; <a href="/jspui/browse?type=author&amp;value=Kasyanenko%2C+Sonya">Kasyanenko,&#x20;Sonya</a>; <a href="/jspui/browse?type=author&amp;value=Al+kaabi%2C+Meera">Al&#x20;kaabi,&#x20;Meera</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="evenRowEvenCol" nowrap="nowrap" align="right">28-Nov-2011</td>
-                <td headers="t2" class="evenRowOddCol"><a href="/jspui/handle/2451/31326">Field&#x20;Methods&#x20;2011-11-16</a></td>
-                <td headers="t3" class="evenRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Kuhn%2C+Jeremy">Kuhn,&#x20;Jeremy</a>; <a href="/jspui/browse?type=author&amp;value=Martin%2C+Sean">Martin,&#x20;Sean</a>; <a href="/jspui/browse?type=author&amp;value=Martin%2C+Sean">Martin,&#x20;Sean</a>; <a href="/jspui/browse?type=author&amp;value=Kuhn%2C+Jeremy">Kuhn,&#x20;Jeremy</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Rayhan">Beksultanova,&#x20;Rayhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="oddRowEvenCol" nowrap="nowrap" align="right">25-Nov-2011</td>
-                <td headers="t2" class="oddRowOddCol"><a href="/jspui/handle/2451/31325">Field&#x20;Methods&#x20;2011-11-16</a></td>
-                <td headers="t3" class="oddRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Chodroff%2C+Eleanor">Chodroff,&#x20;Eleanor</a>; <a href="/jspui/browse?type=author&amp;value=Martin%2C+Sean">Martin,&#x20;Sean</a>; <a href="/jspui/browse?type=author&amp;value=Kasyanenko%2C+Sonia">Kasyanenko,&#x20;Sonia</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raikhan">Beksultanova,&#x20;Raikhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="evenRowEvenCol" nowrap="nowrap" align="right">24-Nov-2011</td>
-                <td headers="t2" class="evenRowOddCol"><a href="/jspui/handle/2451/31324">Field&#x20;Methods&#x20;2011-11-23</a></td>
-                <td headers="t3" class="evenRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Kuhn%2C+Jeremy">Kuhn,&#x20;Jeremy</a>; <a href="/jspui/browse?type=author&amp;value=Kasyanenko%2C+Sonia">Kasyanenko,&#x20;Sonia</a>; <a href="/jspui/browse?type=author&amp;value=Lee%2C+Sang-Im">Lee,&#x20;Sang-Im</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="oddRowEvenCol" nowrap="nowrap" align="right">21-Nov-2011</td>
-                <td headers="t2" class="oddRowOddCol"><a href="/jspui/handle/2451/31323">Field&#x20;Methods&#x20;2011-11-09</a></td>
-                <td headers="t3" class="oddRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Kuhn%2C+Jeremy">Kuhn,&#x20;Jeremy</a>; <a href="/jspui/browse?type=author&amp;value=Martin%2C+Sean">Martin,&#x20;Sean</a>; <a href="/jspui/browse?type=author&amp;value=Martin%2C+Sean">Martin,&#x20;Sean</a>; <a href="/jspui/browse?type=author&amp;value=Kuhn%2C+Jeremy">Kuhn,&#x20;Jeremy</a>; <a href="/jspui/browse?type=author&amp;value=Martin%2C+Sean">Martin,&#x20;Sean</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="evenRowEvenCol" nowrap="nowrap" align="right">21-Nov-2011</td>
-                <td headers="t2" class="evenRowOddCol"><a href="/jspui/handle/2451/31322">Class&#x20;Session&#x20;2011&#x2F;11&#x2F;09</a></td>
-                <td headers="t3" class="evenRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Kaufman%2C+Brianna">Kaufman,&#x20;Brianna</a>; <a href="/jspui/browse?type=author&amp;value=Lowy%2C+Maya">Lowy,&#x20;Maya</a>; <a href="/jspui/browse?type=author&amp;value=Martin%2C+Sean">Martin,&#x20;Sean</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="oddRowEvenCol" nowrap="nowrap" align="right">20-Nov-2011</td>
-                <td headers="t2" class="oddRowOddCol"><a href="/jspui/handle/2451/31321">Field&#x20;Session&#x20;2011-11-14</a></td>
-                <td headers="t3" class="oddRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Lee%2C+Sang-Im">Lee,&#x20;Sang-Im</a>; <a href="/jspui/browse?type=author&amp;value=Kaufman%2C+Brianna">Kaufman,&#x20;Brianna</a>; <a href="/jspui/browse?type=author&amp;value=Kaufman%2C+Brianna">Kaufman,&#x20;Brianna</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="evenRowEvenCol" nowrap="nowrap" align="right">18-Nov-2011</td>
-                <td headers="t2" class="evenRowOddCol"><a href="/jspui/handle/2451/31319">Field&#x20;Methods&#x20;2011-11-14</a></td>
-                <td headers="t3" class="evenRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Martin%2C+Sean">Martin,&#x20;Sean</a>; <a href="/jspui/browse?type=author&amp;value=Kaufman%2C+Brianna">Kaufman,&#x20;Brianna</a>; <a href="/jspui/browse?type=author&amp;value=Chodroff%2C+Eleanor">Chodroff,&#x20;Eleanor</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-              <tr>
-                <td headers="t1" class="oddRowEvenCol" nowrap="nowrap" align="right">18-Nov-2011</td>
-                <td headers="t2" class="oddRowOddCol"><a href="/jspui/handle/2451/31318">Field&#x20;Session&#x20;2011-11-10</a></td>
-                <td headers="t3" class="oddRowEvenCol"><em><a href="/jspui/browse?type=author&amp;value=Lowy%2C+Maya">Lowy,&#x20;Maya</a>; <a href="/jspui/browse?type=author&amp;value=Chodroff%2C+Eleanor">Chodroff,&#x20;Eleanor</a>; <a href="/jspui/browse?type=author&amp;value=Lowy%2C+Maya">Lowy,&#x20;Maya</a>; <a href="/jspui/browse?type=author&amp;value=Beksultanova%2C+Raykhan">Beksultanova,&#x20;Raykhan</a></em></td>
-              </tr>
-            </table>
-            <div class="browse_range">
-              Collection&#39;s Items (Sorted by Submit Date in Descending order): 1 to 20 of 71
-            </div>
-            <div class="prev-next-links">
-              &nbsp;<a href="/jspui/handle/2451/29947?offset=20">next&nbsp;&gt;</a>
-            </div>
+              <div class="prev-next-links">
+                &nbsp;<a href="/jspui/handle/2451/29947?offset=20">next&nbsp;&gt;</a>
+              </div>
+              <?php include "../../inc/collectionitems.php";  ?>
+                <div class="browse_range">
+                  Collection&#39;s Items (Sorted by Submit Date in Descending order): 1 to 20 of 71
+                </div>
+                <div class="prev-next-links">
+                  &nbsp;<a href="/jspui/handle/2451/29947?offset=20">next&nbsp;&gt;</a>
+                </div>
           </div>
           <div class="col-md-3">
             <div class="panel panel-admin-tools">
@@ -306,10 +172,9 @@
                 </div>
               </div>
             </div>
-           <?php include "../../inc/panel-subscribe.php";  ?>
+            <?php include "../../inc/panel-subscribe.php";  ?>
           </div>
         </div>
-      </div>
     </main>
     <?php include "../../inc/footer.php";  ?>
 </body>
---
+++ jspui/inc/searchcollection.php
@@ -0,0 +1,15 @@
+<div class="panel panel-primary">
+ <!--  <div class="panel-heading">Search within this community:</div> -->
+  <div class="panel-body">
+    <form method="get" action="/jspui/simple-search" class="simplest-search">
+      <div class="form-group-flex">
+        <div class="input-hold">
+          <input type="text" class="form-control" placeholder="Titles, authors, keywords..." name="query" id="tequery">
+        </div>
+        <div class="button-hold">
+          <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span></button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
